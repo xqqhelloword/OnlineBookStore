43c604b8a1ef4011d5ee50955220cf1eb82386b3
xqq <1536801671@qq.com>
2018-07-09 22:18:23 +0800

    bookList模块完成
    修复了一个bug：
    json格式数据数组嵌套前端接收不到。

--- src/dao/DbMethod.java
+++ src/dao/DbMethod.java
@@ -11,6 +11,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 import db.connDB;
+import bean.Ebook;
+import bean.Obook;
+import bean.Pbook;
+import bean.User;
 import bean.UserBean;
 
 public class DbMethod {
@@ -49,9 +53,139 @@ public class DbMethod {
 		}
 			return objArrayL;
 		}
-	
+	public ArrayList<Pbook> searchPbook(){
+			ArrayList<Pbook> objArrayL=new ArrayList<Pbook>();
+			ResultSet rs = null;
+			Statement ps=null;
+			String sql="select * from pbook ";
+			System.out.println("searchPook查询语句:"+sql);
+			try{
+				ps = conn.createStatement();
+			 rs=ps.executeQuery(sql);
+			while(rs.next()){
+				Pbook pb=new Pbook();
+				pb.setIdPbook(rs.getString("idPbook"));
+				pb.setPbookAbstract(rs.getString("PbookAbstract"));
+				pb.setPbookClickTimes(rs.getString("PbookClickTimes"));
+				pb.setPbookName(rs.getString("PbookName"));
+				pb.setPbookPictureUrl(rs.getString("PbookPictureUrl"));
+				pb.setPbookPrice(rs.getString("PbookPrice"));
+				pb.setPbookPublisher(rs.getString("PbookPublisher"));
+				pb.setPbookPublishTime(rs.getString("PbookPublishTime"));
+				pb.setPbookSoldNumber(rs.getString("PbookSoldNumber"));
+				pb.setPbookStockNumber(rs.getString("PbookStockNumber"));
+				pb.setPbookWriter(rs.getString("PbookWriter"));
+				objArrayL.add(pb);
+			}
+			}catch(SQLException e){
+				e.printStackTrace();
+				System.out.println("\nsearch pbook Fail--[sqlException]:"+" "+e.getMessage()+"\n");
+				
+			}finally{
+				connDB.frees( ps, rs);
+			}	
+			
+			return objArrayL;
+	}
+	public ArrayList<Ebook> searchebook(){
+		ArrayList<Ebook> objArrayL=new ArrayList<Ebook>();
+		ResultSet rs = null;
+		Statement ps=null;
+		String sql="select * from ebook ";
+		System.out.println("searcheook查询语句:"+sql);
+		try{
+			ps = conn.createStatement();
+		 rs=ps.executeQuery(sql);
+		while(rs.next()){
+			Ebook pb=new Ebook();
+			pb.setIdebook(rs.getString("idebook"));
+			pb.setEbookAbstract(rs.getString("ebookAbstract"));
+			pb.setEbookClickTimes(rs.getString("ebookClickTimes"));
+			pb.setEbookPictureUrl(rs.getString("ebookPictureUrl"));
+			pb.setEbookPrice(rs.getString("ebookPrice"));
+			pb.setEbookSoldNumber(rs.getString("ebookSoldNumber"));
+			pb.setEbookWriter(rs.getString("ebookWriter"));
+			pb.setEbookNameS(rs.getString("ebookName"));
+			objArrayL.add(pb);
+		}
+		}catch(SQLException e){
+			e.printStackTrace();
+			System.out.println("\nsearchebook Fail--[sqlException]:"+" "+e.getMessage()+"\n");
+			
+		}finally{
+			connDB.frees( ps, rs);
+		}	
+		return objArrayL;
+}
+	public ArrayList<Obook> searchobook(){
+		ArrayList<Obook> objArrayL=new ArrayList<Obook>();
+		ResultSet rs = null;
+		Statement ps=null;
+		String sql="select * from obook ";
+		System.out.println("searchObook查询语句:"+sql);
+		try{
+			ps = conn.createStatement();
+		 rs=ps.executeQuery(sql);
+		while(rs.next()){
+			Obook pb=new Obook();
+			pb.setIdobook(rs.getString("idobook"));
+			pb.setObookAbstract(rs.getString("obookAbstract"));
+			pb.setObookClickTimes(rs.getString("obookClickTimes"));
+			pb.setObookName(rs.getString("obookName"));
+			pb.setObookPictureUrl(rs.getString("obookPictureUrl"));
+			pb.setObookPrice(rs.getString("obookPrice"));
+			pb.setObookPublisher(rs.getString("obookPublisher"));
+			pb.setObookPublishTime(rs.getString("obookPublishTime"));
+			pb.setObookSoldNumber(rs.getString("obookSoldNumber"));
+			pb.setObookStockNumber(rs.getString("obookStockNumber"));
+			pb.setObookWriter(rs.getString("obookWriter"));
+			objArrayL.add(pb);
+		}
+		}catch(SQLException e){
+			e.printStackTrace();
+			System.out.println("\nsearchobook Fail--[sqlException]:"+" "+e.getMessage()+"\n");
+			
+		}finally{
+			connDB.frees( ps, rs);
+		}	
+		return objArrayL;
+}
+	public ArrayList<Pbook> searchTopList() {
+		ArrayList<Pbook> topList=new ArrayList<Pbook>();
+		ResultSet rs = null;
+		Statement ps=null;
+		String sql="select * from pbook order by PbookSoldNumber desc limit 0,2";
+		System.out.println("searchPook查询语句:"+sql);
+		try{
+			ps = conn.createStatement();
+		 rs=ps.executeQuery(sql);
+		while(rs.next()){
+			Pbook pb=new Pbook();
+			pb.setIdPbook(rs.getString("idPbook"));
+			pb.setPbookAbstract(rs.getString("PbookAbstract"));
+			pb.setPbookClickTimes(rs.getString("PbookClickTimes"));
+			pb.setPbookName(rs.getString("PbookName"));
+			pb.setPbookPictureUrl(rs.getString("PbookPictureUrl"));
+			pb.setPbookPrice(rs.getString("PbookPrice"));
+			pb.setPbookPublisher(rs.getString("PbookPublisher"));
+			pb.setPbookPublishTime(rs.getString("PbookPublishTime"));
+			pb.setPbookSoldNumber(rs.getString("PbookSoldNumber"));
+			pb.setPbookStockNumber(rs.getString("PbookStockNumber"));
+			pb.setPbookWriter(rs.getString("PbookWriter"));
+			topList.add(pb);
+		}
+		}catch(SQLException e){
+			e.printStackTrace();
+			System.out.println("\nsearch pbook Fail--[sqlException]:"+" "+e.getMessage()+"\n");
+			
+		}finally{
+			connDB.frees( ps, rs);
+		}	
+		return topList;
+
+		// TODO Auto-generated method stub
+	}
 	public  boolean insert(String sql, Object... args) {
-		Connection conn = null;
 		PreparedStatement ps = null;
 		try {
 			conn = connDB.getConnection();
@@ -66,7 +200,7 @@ public class DbMethod {
 			System.out.println("\n[insertsqlException]:"+" "+e.getMessage()+"\n");
 			return false;
 		}finally {
-			connDB.freeAll(conn, ps, null);
+			connDB.frees( ps, null);
 		}
 		return false;
 
@@ -74,8 +208,6 @@ public class DbMethod {
 	public  boolean update(String sql, Object... args) {
 		Connection conn = null;
 		PreparedStatement ps = null;
-		Object[] dd={"1","2"};
-		String sql2="update xxx set xx=xxx,xxx==xxx where xxx=xxx" ;
 		try {
 			conn = connDB.getConnection();
 			ps = conn.prepareStatement(sql);
@@ -89,7 +221,7 @@ public class DbMethod {
 			System.out.println("\n[updatesqlException]:"+" "+e.getMessage()+"\n");
 			return false;
 		}finally {
-			connDB.freeAll(conn, ps, null);
+			connDB.frees( ps, null);
 		}
 		return false;
 
